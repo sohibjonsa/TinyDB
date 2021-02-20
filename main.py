b1140ebd72c570cc0e8be66599beec9ddeaaa3d7
sohibjonsa <sohibjonsa@gmail.com>
2021-02-20 23:56:50 +0500

    beginning

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
+
 db = TinyDB('db.json')
-
 products = open('products.csv').read().split('\n')
-
-products.pop(0)
-
+print(products)
 for row in products:
     print(row)
-
-# sm1 = {'ID': 1, 'category': 'smartphone', 'company': 'Samsung'}
-# sm2 = {'ID': 2, 'category': 'smartphone', 'company': 'Apple'}
-# sm3 = {'ID': 3, 'category': 'smartphone', 'company': 'Huawei'}
-# sm4 = {'ID': 4, 'category': 'smartphone', 'company': 'Oppo'}
-# sm5 = {'ID': 5, 'category': 'smartphone', 'company': 'Xiaomi'}
-
-
-
-# db.insert(sm1)
-# db.insert(sm2)
-# db.insert(sm3)
-# db.insert(sm4)
-# db.insert(sm5)
